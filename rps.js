b4feea84cfb6ff32142eaae9534ef2fe38bfe757
Etko <etkomccooty16@gmail.com>
2021-05-07 15:16:55 +0100

    Return statements now display messages to the console; previous console messages deleted

--- rps.js
+++ rps.js
@@ -16,24 +16,21 @@ let playRound = (playerSelection, computerSelection) => {
     if ((playerSelection === 'rock' && computerSelection === 'scissors') || 
         (playerSelection === 'paper' && computerSelection === 'rock') ||
         (playerSelection === 'scissors' && computerSelection === 'paper')) {
-            console.log('Player Wins');
             playerScore++;
-            return `You Win! ${playerSelection} beats ${computerSelection}!`;
+            return console.log(`You Win! ${playerSelection} beats ${computerSelection}!`);
         }
     // For lose conditions
     else if ((playerSelection === 'rock' && computerSelection === 'paper') ||
              (playerSelection === 'paper' && computerSelection === 'scissors') ||
              (playerSelection === 'scissors' && computerSelection === 'rock')) {
-                console.log('CPU Wins!');
                 computerScore++;
-                return `You Lose! ${computerSelection} beats ${playerSelection}!`;
+                return console.log(`You Lose! ${computerSelection} beats ${playerSelection}!`);
         }
     // For a draw
     else {
-        console.log('Draw!');
         playerScore++;
         computerScore++;
-        return 'It\'s a draw!';
+        return console.log('It\'s a draw!');
     }
 }
 
@@ -51,10 +48,9 @@ function game () {
     // Determine Winner
     let message = (playerScore > computerScore) ? `Player: ${playerScore}\nCPU: ${computerScore}\n\nPlayer Wins!` : 
                  (playerScore < computerScore) ? `Player: ${playerScore}\nCPU: ${computerScore}\n\nCPU Wins!` :
-                 `Player: ${playerScore}\nCPU: ${computerScore}\n\nIt\'s a draw!`
+                 `Player: ${playerScore}\nCPU: ${computerScore}\n\nBoth players draw the same amount of games resulting in a draw!`
 
-    console.log(message);
-    return message;
+    return console.log(message); 
 }
 
 game();
